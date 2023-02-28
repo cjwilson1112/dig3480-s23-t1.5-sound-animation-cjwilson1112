d8336c2f7daa03f00701955211e11e2a08d9a744
wildcardgamez <122412919+cjwilson1112@users.noreply.github.com>
2023-02-28 13:08:09 -0500

    Revert "Assignment"
    This reverts commit 9fadf098698fb41f4cca005aad4488c2c5937eb7.

--- Assets/AudioPlayer.cs
+++ Assets/AudioPlayer.cs
@@ -2,38 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatController : MonoBehaviour
+public class AudioPlayer : MonoBehaviour
 {
     public AudioClip musicClipOne;
     public AudioClip musicClipTwo;
     public AudioSource musicSource;
-    Animator anim;
 
-    void Start() {
-        anim = GetComponent<Animator>();
-    }
-
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.W)) {
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)) 
+        {
             musicSource.clip = musicClipOne;
             musicSource.Play();
-            anim.SetInteger("State", 1);
         }
-        if (Input.GetKeyUp(KeyCode.W)) {
+
+        if (Input.GetKeyUp(KeyCode.W))
             musicSource.Stop();
-            anim.SetInteger("State", 0);
-        }
-        if (Input.GetKeyDown(KeyCode.R)) {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
             musicSource.clip = musicClipTwo;
             musicSource.Play();
-            anim.SetInteger("State", 2);
         }
-        if (Input.GetKeyUp(KeyCode.R)) {
+
+        if (Input.GetKeyUp(KeyCode.R))
             musicSource.Stop();
-            anim.SetInteger("State", 0);
-        }
+
         if (Input.GetKeyDown(KeyCode.L))
             musicSource.loop = true;
+
         if (Input.GetKeyUp(KeyCode.L))
             musicSource.loop = false;
     }
